39348dc684faefb56eb463362a975b0ae5716b33
Nathaniel Odell <naodell@gmail.com>
2016-04-20 11:03:09 -0500

    formalizing project

--- setup.py
+++ setup.py
@@ -1,5 +1,17 @@
-from distutils.core import setup
-setup(name='foo',
-      version='1.0',
-      py_modules=['foo'],
-      )
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+def readme():
+  with open('README.md') as f:
+    return f.read()
+
+setup(name='NLLFitter',
+    version='0.1',
+    description='NLL fitting tools for bump hunting.',
+    url='https://github.com/naodell/amumu',
+    author='Nathaniel Odell',
+    author_email='naodell@gmail.com',
+    license='NU',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False)
